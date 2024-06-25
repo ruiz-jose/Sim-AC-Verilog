350e2e03c1abfcdce404638c607ad2ce3f30d116
jose-ruiz <jose.ruiz@uner.edu.ar>
2024-06-25 19:31:24 -0300

    correcciones ldi

--- src/cpu.v
+++ src/cpu.v
@@ -19,15 +19,15 @@ module cpu(
   output wr_o, wm_o
 
 );
-  wire [7:0] bus_ac_mem, bus_alu, bus_mem, bus_ac;
+  wire [7:0] bus_alux, bus_aluy, bus_alur, bus_mem, bus_ac;
   wire pc_load;
-  wire ctrl_jmp, ctrl_wm, ctrl_wf, ctrl_aluop, ctrl_ldi, ctrl_wr, ctrl_alux;
-  wire [1:0] flags, bus_sw;
+  wire ctrl_alux, ctrl_aluop, ctrl_wr, ctrl_wm, ctrl_jmp, ctrl_wf, ctrl_ldi;
+  wire [1:0] bus_flags, bus_sw;
 
   // registers
-  reg8 reg_acc(.clk_i(clk_i), .rst_i(reset_i), .wen_i(ctrl_wr), .d_i(bus_alu), .q_o(bus_ac));
+  reg8 reg_acc(.clk_i(clk_i), .rst_i(reset_i), .wen_i(ctrl_wr), .d_i(bus_alur), .q_o(bus_ac));
   pc reg_pc(.clk_i(clk_i), .rst_i(reset_i), .jmp_en_i(pc_load), .jmp_addr_i(curr_ins[4:0]), .addr_o(curr_pc));
-  regSW reg_SW(.clk_i(clk_i), .rst_i(reset_i), .wen_i(ctrl_wf), .d_i(flags), .q_o(bus_sw));
+  regSW reg_SW(.clk_i(clk_i), .rst_i(reset_i), .wen_i(ctrl_wf), .d_i(bus_flags), .q_o(bus_sw));
 
   // top level modules
   rom rom(.addr_i(curr_pc), .data_o(curr_ins));
@@ -35,10 +35,12 @@ module cpu(
 
   branch branch(.op_i(curr_ins[7:5]), .flags_i(bus_sw), .ctrl_jmp_i(ctrl_jmp), .branch_o(pc_load));
   ram ram(.clk_i(clk_i), .wen_i(ctrl_wm), .din_i(bus_ac), .addr_i(curr_ins[4:0]), .dout_o(bus_mem));
-  alu alu(.x_i(bus_ac_mem), .y_i( (ctrl_ldi) ? {3'b0, curr_ins[4:0]}: bus_mem), .op_i(curr_ins[7:5]), .r_o(bus_alu), .flags_o(flags));
+  alu alu(.x_i(bus_alux), .y_i( bus_aluy), .op_i(curr_ins[7:5]), .r_o(bus_alur), .flags_o(bus_flags));
+
 
   // assign bus values  
-  assign bus_ac_mem = (ctrl_alux) ? 8'b0 :bus_ac;
+  assign bus_alux = (ctrl_alux) ? 8'b0 : bus_ac;
+  assign bus_aluy = (ctrl_ldi) ? 8'b0 : bus_mem;
 
   // assign outputs
   assign reg_acc_o = bus_ac; 
